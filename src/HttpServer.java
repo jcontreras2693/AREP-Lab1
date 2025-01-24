72127758658afc15df5f13255d71f23235f8112e
unknown <juan.cbecerra@labinfo.is.escuelaing.edu.co>
2025-01-24 09:44:20 -0500

    Class Work

--- src/HttpServer.java
+++ src/HttpServer.java
@@ -49,58 +49,59 @@ public class HttpServer {
                 outputLine = helloRestService(requestedFile.getPath(), requestedFile.getQuery());
                 out.println(outputLine);
             } else{
-                outputLine = "HTTP/1.1 200 OKc\r\n"
-                        + "Content-Type: text/html\r\n"
-                        + "\r\n"
-                        + "<!DOCTYPE html>\n" +
-                        "<html>\n" +
-                        "    <head>\n" +
-                        "        <title>Form Example</title>\n" +
-                        "        <meta charset=\"UTF-8\">\n" +
-                        "        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
-                        "    </head>\n" +
-                        "    <body>\n" +
-                        "        <h1>Form with GET</h1>\n" +
-                        "        <form action=\"/app/hello\">\n" +
-                        "            <label for=\"name\">Name:</label><br>\n" +
-                        "            <input type=\"text\" id=\"name\" name=\"name\" value=\"John\"><br><br>\n" +
-                        "            <input type=\"button\" value=\"Submit\" onclick=\"loadGetMsg()\">\n" +
-                        "        </form> \n" +
-                        "        <div id=\"getrespmsg\"></div>\n" +
-                        "\n" +
-                        "        <script>\n" +
-                        "            function loadGetMsg() {\n" +
-                        "                let nameVar = document.getElementById(\"name\").value;\n" +
-                        "                const xhttp = new XMLHttpRequest();\n" +
-                        "                xhttp.onload = function() {\n" +
-                        "                    document.getElementById(\"getrespmsg\").innerHTML =\n" +
-                        "                    this.responseText;\n" +
-                        "                }\n" +
-                        "                xhttp.open(\"GET\", \"/app/hello?name=\"+nameVar);\n" +
-                        "                xhttp.send();\n" +
-                        "            }\n" +
-                        "        </script>\n" +
-                        "\n" +
-                        "        <h1>Form with POST</h1>\n" +
-                        "        <form action=\"/app/hello\">\n" +
-                        "            <label for=\"postname\">Name:</label><br>\n" +
-                        "            <input type=\"text\" id=\"postname\" name=\"name\" value=\"John\"><br><br>\n" +
-                        "            <input type=\"button\" value=\"Submit\" onclick=\"loadPostMsg(postname)\">\n" +
-                        "        </form>\n" +
-                        "        \n" +
-                        "        <div id=\"postrespmsg\"></div>\n" +
-                        "        \n" +
-                        "        <script>\n" +
-                        "            function loadPostMsg(name){\n" +
-                        "                let url = \"/app/hello?name=\" + name.value;\n" +
-                        "\n" +
-                        "                fetch (url, {method: 'POST'})\n" +
-                        "                    .then(x => x.text())\n" +
-                        "                    .then(y => document.getElementById(\"postrespmsg\").innerHTML = y);\n" +
-                        "            }\n" +
-                        "        </script>\n" +
-                        "    </body>\n" +
-                        "</html>";
+                outputLine = """
+                        HTTP/1.1 200 OKc\r
+                        Content-Type: text/html\r
+                        \r
+                        <!DOCTYPE html>
+                        <html>
+                            <head>
+                                <title>Form Example</title>
+                                <meta charset="UTF-8">
+                                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                            </head>
+                            <body>
+                                <h1>Form with GET</h1>
+                                <form action="/app/hello">
+                                    <label for="name">Name:</label><br>
+                                    <input type="text" id="name" name="name" value="John"><br><br>
+                                    <input type="button" value="Submit" onclick="loadGetMsg()">
+                                </form>\s
+                                <div id="getrespmsg"></div>
+
+                                <script>
+                                    function loadGetMsg() {
+                                        let nameVar = document.getElementById("name").value;
+                                        const xhttp = new XMLHttpRequest();
+                                        xhttp.onload = function() {
+                                            document.getElementById("getrespmsg").innerHTML =
+                                            this.responseText;
+                                        }
+                                        xhttp.open("GET", "/app/hello?name="+nameVar);
+                                        xhttp.send();
+                                    }
+                                </script>
+
+                                <h1>Form with POST</h1>
+                                <form action="/app/hello">
+                                    <label for="postname">Name:</label><br>
+                                    <input type="text" id="postname" name="name" value="John"><br><br>
+                                    <input type="button" value="Submit" onclick="loadPostMsg(postname)">
+                                </form>
+                               \s
+                                <div id="postrespmsg"></div>
+                               \s
+                                <script>
+                                    function loadPostMsg(name){
+                                        let url = "/app/hello?name=" + name.value;
+
+                                        fetch (url, {method: 'POST'})
+                                            .then(x => x.text())
+                                            .then(y => document.getElementById("postrespmsg").innerHTML = y);
+                                    }
+                                </script>
+                            </body>
+                        </html>""";
                 out.println(outputLine);
             }
 
@@ -112,10 +113,11 @@ public class HttpServer {
     }
 
     private static String helloRestService(String path, String query) {
-        String response = "HTTP/1.1 200 OKc\r\n"
-                + "Content-Type: application/json\r\n"
-                + "\r\n"
-                + "{\"name\":\"John\", \"age\":30, \"car\":null}";
+        String response = """
+                HTTP/1.1 200 OKc\r
+                Content-Type: application/json\r
+                \r
+                {"name":"John", "age":30, "car":null}""";
         return response;
     }
 }
