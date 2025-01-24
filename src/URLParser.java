08eead00214d502e32111a6409150b740c2b21c9
unknown <juan.cbecerra@labinfo.is.escuelaing.edu.co>
2025-01-24 08:32:13 -0500

    Break Commit

---
+++ src/URLParser.java
@@ -0,0 +1,20 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.io.*;
+import java.security.spec.RSAOtherPrimeInfo;
+
+public class URLParser {
+    public static void main(String[] args) throws MalformedURLException {
+        URL myUrl = new URL("https://www.escuelaing.edu.co/es/programas/ingenieria-de-sistemas/#scrolling-content-plan_de_estudios-6");
+
+        System.out.println("Protocol: " + myUrl.getProtocol());
+        System.out.println("Authority: " + myUrl.getAuthority());
+        System.out.println("Host: " + myUrl.getHost());
+        System.out.println("Port: " + myUrl.getPort());
+        System.out.println("Path: " + myUrl.getPath());
+        System.out.println("Query: " + myUrl.getQuery());
+        System.out.println("File: " + myUrl.getFile());
+        System.out.println("Ref: " + myUrl.getRef());
+
+    }
+}
